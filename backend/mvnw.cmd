7b07a262f8cd7828aafbfde00961e2c6c25a892b
Thiago Dias <thiago.dsd@outlook.com>
2026-03-21 13:31:10 -0300

    Update .gitignore and Makefile to use Maven wrapper for build commands

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -1,11 +1,14 @@
-@REM Maven Wrapper script for Windows
 @echo off
-set MAVEN_PROJECTBASEDIR=%~dp0
-set MAVEN_WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar
+setlocal
 
-if exist "%MAVEN_WRAPPER_JAR%" (
-    java -jar "%MAVEN_WRAPPER_JAR%" %*
-) else (
-    echo Maven wrapper jar not found at %MAVEN_WRAPPER_JAR%
-    exit /b 1
+set "MAVEN_HOME=%~dp0.mvn\maven\apache-maven-3.9.6"
+
+if not exist "%MAVEN_HOME%\bin\mvn.cmd" (
+    echo Downloading Maven 3.9.6...
+    mkdir "%~dp0.mvn\maven" 2>nul
+    curl -sL https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.6/apache-maven-3.9.6-bin.zip -o "%~dp0.mvn\maven\maven.zip"
+    powershell -Command "Expand-Archive -Force '%~dp0.mvn\maven\maven.zip' '%~dp0.mvn\maven'"
+    del "%~dp0.mvn\maven\maven.zip"
 )
+
+"%MAVEN_HOME%\bin\mvn.cmd" %*
